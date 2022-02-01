b132b51fb72749483c9195784197ee664f0ca8cc
glagotskiyse <glagotskiyse@gmail.com>
2022-02-02 01:15:02 +0200

    hw1

---
+++ lesson 1/main.js
@@ -0,0 +1,149 @@
+/*- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+  Вивести кожну змінну за допомогою: console.log , alert, document.write*/
+/*{
+    let ten = 10;
+    console.log(ten);
+    alert(ten);
+    document.write(ten);
+    let minus = -999;
+    console.log(minus);
+    alert(minus);
+    document.write(minus);
+    let caunt = 123;
+    console.log(caunt);
+    alert(caunt);
+    document.write(caunt);
+    let pi = 3.14;
+    console.log(pi);
+    alert(pi);
+    document.write(pi);
+    let bar = 2.7;
+    console.log(bar);
+    alert(pi);
+    document.write(bar);
+    let passport = 16;
+    console.log(passport);
+    alert(passport);
+    document.write(passport);
+    let pravda = true;
+    console.log(pravda);
+    alert(pravda);
+    document.write(pravda);
+    let loj = false;
+    console.log(loj);
+    alert(loj);
+    document.write(loj)
+
+}*/
+//- Переприсвоїти кожній змінній з завдання значення на довільне.
+//   Вивести кожну змінну за допомогою: console.log , alert, document.write
+/*{
+    let privet = `privet`;
+    console.log(privet);
+    alert(privet);
+    document.write(privet);
+    let study = `univer`;
+    console.log(study);
+    alert(study);
+    document.write(study);
+    let site = `in`;
+    console.log(site);
+    alert(site);
+    document.write(site);
+    let ukr = `ua`;
+    console.log(ukr);
+    alert(ukr);
+    document.write(ukr);
+    let one = 11;
+    console.log(one);
+    alert(one);
+    document.write(one);
+    let ten = 1010;
+    console.log(ten);
+    alert(ten);
+    document.write(ten);
+    let minus = -444;
+    console.log(minus);
+    alert(minus);
+    document.write(minus);
+    let caunt = 12345;
+    console.log(caunt);
+    alert(caunt);
+    document.write(caunt);
+    let pi = 86;
+    console.log(pi);
+    alert(pi);
+    document.write(pi);
+    let bar = 2.3;
+    console.log(bar);
+    alert(bar);
+    document.write(bar);
+    let passport = 14;
+    console.log(passport);
+    alert(passport);
+    document.write(passport);
+    let pravda = false;
+    console.log(pravda);
+    alert(pravda);
+    document.write(pravda);
+    let loj = true;
+    console.log(loj);
+    alert(loj);
+    document.write(loj)
+}*/
+
+//- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+/*{
+    let firstName = `Сергій`;
+    let middleName = `Миколайович`;
+    let lastName = `Глагоцький`;
+
+    let person = [firstName, middleName, lastName];
+    console.log(person);
+
+    let promt = {
+    first: firstName,
+    middle: middleName,
+    age: 35
+    }
+    console.log(promt.first);
+    console.log(promt.middle);
+    console.log(promt.age);
+    console.log(`Вітаю`, promt.first, promt.middle, `Тобі`, promt.age, `років.`)
+}*/
+//- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = '100'; let c = true;
+/*
+{
+    let a = 100;
+    console.log(typeof 100);
+    let b = '100';
+    console.log(typeof b);
+    let c = true;
+    console.log(typeof c);
+}*/
+
+//- Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//  В однакових виразах не використовувати однакові оператори!!!
+/*
+let one = 5 < 6;
+let two = 5 > 6;
+let three = 5 === 6;
+let four = 5 >= 6;
+let five = 10 === 10;
+let six = 10 >= 10;
+let seven = 10 !== 10;
+let eight = 10 > 10;
+let nine = 10 < 10;
+let ten = 123 === '123';
+let eleven = 123 !== `123`;*/
+//- Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+/*
+let str = "20";
+let a = 5;
+document.write(str + a + "<br/>"); //- 205
+document.write(str - a + "<br/>"); // - 15
+document.write(str * "2" + "<br/>"); //- 20*2
+document.write(str / 2 + "<br/>");// -10*/
